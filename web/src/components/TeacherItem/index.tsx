60e226050a5aefccbee864ec7c26d7361585e2cd
Hicharles Rocha <hicharlesolvr@gmail.com>
2020-08-08 00:27:08 -0300

    Finishing lesson 3, consuming api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,47 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name:string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id
+        })
+    }
     return (
         <article className="teacher-item">
         <header>
-            <img src="https://avatars3.githubusercontent.com/u/13335847?s=460&u=76d5c2b43ea8d1e75e7d8a3c0426d457aa8fce8e&v=4" alt="Hicharles Rocha"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>Hicharles Rocha</strong>
-                <span>Química</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
 
-        <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /><br />
-            Apaixonado por explodir coisas em laboratórios e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões
-        </p>
+        <p>{teacher.bio}</p>
         <footer>
             <p>Preço/hora
-            <strong>R$ 80,00</strong>
+            <strong>R$ {teacher.cost}</strong>
             </p>
-            <button type="button">
-            <img src={whatsappIcon} alt="Whatsapp"/>
-            Entrar em contato
-        </button>
+            <a onClick={createNewConnection} target="_blank" href={`https://wa.me/${teacher.whatsapp}`} type="button">
+                <img src={whatsappIcon} alt="Whatsapp"/>
+                Entrar em contato
+            </a>
         </footer>
     </article>
     );
